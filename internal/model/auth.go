d56f0a727bde257b7ff3781f881780874f295f12
r021n <rozinabdul25@gmail.com>
2026-02-27 11:18:43 +0700

    Add authentication models for user registration and login requests

---
+++ internal/model/auth.go
@@ -0,0 +1,17 @@
+package model
+
+type RegisterRequest struct {
+	Name     string `json:"name" validate:"required,min=3,max=255"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=100"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
