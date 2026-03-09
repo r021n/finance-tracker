2c38efe3074c433fe13d3fe2e0b97ddee292ec6f
r021n <rozinabdul25@gmail.com>
2026-03-09 15:09:06 +0700

    Implement FindByID, FindByName, Update, and Delete methods in CategoryRepository

--- internal/repository/category_repository.go
+++ internal/repository/category_repository.go
@@ -23,3 +23,29 @@ func (r *CategoryRepository) FindAll() ([]model.Category, error) {
 	err := r.db.Order("id asc").Find(&categories).Error
 	return categories, err
 }
+
+func (r *CategoryRepository) FindByID(id uint) (*model.Category, error) {
+	var category model.Category
+	err := r.db.First(&category, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (r *CategoryRepository) FindByName(name string) (*model.Category, error) {
+	var category model.Category
+	err := r.db.Where("name = ?", name).First(&category).Error
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (r *CategoryRepository) Update(category *model.Category) error {
+	return r.db.Save(category).Error
+}
+
+func (r CategoryRepository) Delete(id uint) error {
+	return r.db.Delete(&model.Category{}, id).Error
+}
